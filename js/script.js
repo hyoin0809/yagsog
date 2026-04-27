abdc7869031c5ef688170e8bbf2627e85311f0bd
hyoin0809 <hi589913@naver.com>
2026-04-27 17:51:00 +0900

    디자인구현

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-window.addEventListener("load", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".option-card");
+
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      cards.forEach((item) => item.classList.remove("selected"));
+      card.classList.add("selected");
+    });
+  });
+
+  const nextBtn = document.querySelector("#nextBtn");
+
+  if (nextBtn) {
+    nextBtn.addEventListener("click", function () {
+      window.location.href = "main.html";
+    });
+  }
+});
+
+const loginBtn = document.querySelector("#loginBtn");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    window.location.href = "main.html";
+  });
+}
